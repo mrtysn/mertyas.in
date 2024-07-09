b7a9663c8e448633408e6272d9ee010e1f4e7e67
Mert <mert.yasin@gmail.com>
2024-07-09 15:32:15 +0300

    add classless.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,33 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <main>
+      <nav>
+        <ul>
+          <li>Brand</li>
+          <li className="float-right sticky">Sticky Right</li>
+          <li>
+            <a href="#">Item </a>
+          </li>
+          <li>
+            <a href="#">Menu ▾</a>
+            <ul>
+              <li>
+                <a href="#">Menu 1</a>
+              </li>
+              <li>
+                <a href="#">Menu 2</a>
+              </li>
+            </ul>
+          </li>
+          <li className="float-right">Collapse</li>
+        </ul>
+      </nav>
       <h1>Mert Yaşin</h1>
       <div className="card">
         <p>Hello world</p>
       </div>
-    </>
+    </main>
   );
 }
 
